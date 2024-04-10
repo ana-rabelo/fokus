e2c1e13e5adacf39c7edc050f50768f22b3d3d4b
Ana Paula <prabelo79@gmail.com>
2024-04-10 16:01:52 -0300

    Exibe as tarefas no DOM
    Adiciona função de criar tarefa, ler as tarefas salvas no localStorage e esconder o formulário

--- script-crud.js
+++ script-crud.js
@@ -1,10 +1,41 @@
 const btnAddTask = document.querySelector('.app__button--add-task');
 const formAddTask = document.querySelector('.app__form-add-task');
 const btnCancelTask = document.querySelector('.app__form-footer__button--cancel');
-const btnsaveTask = document.querySelector('.app__form-footer__button--confirm')
-const textarea = document.querySelector('.app__form-textarea')
+const btnsaveTask = document.querySelector('.app__form-footer__button--confirm');
+const textarea = document.querySelector('.app__form-textarea');
+const ulTasks = document.querySelector('.app__section-task-list');
 
-const tarefas = [];
+const tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+
+tasks.forEach(task => createTask(task));
+
+function createTask(task) {
+    const li = document.createElement('li');
+    li.classList.add('app__section-task-list-item');
+    
+    const svg = document.createElement('svg');
+    svg.innerHTML = `
+        <svg class="app__section-task-icon-status" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <circle cx="12" cy="12" r="12" fill="#FFF"></circle>
+            <path d="M9 16.1719L19.5938 5.57812L21 6.98438L9 18.9844L3.42188 13.4062L4.82812 12L9 16.1719Z" fill="#01080E"></path>
+        </svg>`;
+
+    const p = document.createElement('p');
+    p.textContent = task.description;
+    p.classList.add('app__section-task-list-item-description');
+
+    const button = document.createElement('button');
+    const imageBtn = document.createElement('img');
+    imageBtn.setAttribute('src', './imagens/edit.png');
+    button.appendChild(imageBtn);
+    button.classList.add('app_button-edit');
+
+    li.appendChild(svg);
+    li.appendChild(p);
+    li.appendChild(button);
+    
+    ulTasks.appendChild(li);
+}
 
 btnAddTask.addEventListener('click', () => {
     formAddTask.classList.toggle('hidden');
@@ -17,18 +48,16 @@ formAddTask.addEventListener('submit', (event) => {
         description: textarea.value,
         completed: false
     };
-    tarefas.push(task);
 
+    tasks.push(task);
     
     /* Para persistir os dados entre sessões */
-    localStorage.setItem('tarefas', JSON.stringify(tarefas));
+    localStorage.setItem('tasks', JSON.stringify(tasks));
     /* O localStorage é similar ao sessionStorage, com a diferença de que enquanto os dados do localStorage 
     não têm tempo de expiração, os dados do sessionStorage são apagados quando a sessão da página termina */
     
-    JSON.parse(localStorage.getItem('tarefas'));
+    createTask(task);
 
-    
-    formAddTask.classList.add('hidden');
     textarea.value = '';
 });
 
