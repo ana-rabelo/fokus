804d4a9434b7668ddc654d54d0b955e3f4d4a451
Ana Paula Rabelo <prabelo79@gmail.com>
2024-04-11 10:55:25 -0300

    Atualiza funcionalidade de editar tarefa

--- script-crud.js
+++ script-crud.js
@@ -1,5 +1,6 @@
 const btnAddTask = document.querySelector('.app__button--add-task');
 const formAddTask = document.querySelector('.app__form-add-task');
+const formLabel = document.querySelector('.app__form-label');
 const btnCancelTask = document.querySelector('.app__form-footer__button--cancel');
 const btnsaveTask = document.querySelector('.app__form-footer__button--confirm');
 const textarea = document.querySelector('.app__form-textarea');
@@ -11,17 +12,28 @@ function updateTask () {
     localStorage.setItem('tasks', JSON.stringify(tasks));
 }
 
+function setId(taskId) {
+    if (document.getElementById('id')) {
+        id.setAttribute('value', taskId);
+    } else {
+        const id = document.createElement('input');
+        id.setAttribute('type', 'hidden');
+        id.setAttribute('id', 'id');
+        id.setAttribute('value', taskId);
+        formAddTask.appendChild(id);  
+    }
+}
+
 function clearForm() {
     formAddTask.classList.add('hidden');
     textarea.value = '';
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-    tasks.forEach(task => createTask(task));
+    tasks.forEach(task => createTask(task));  
 });
 
 function createTask(task) {
-
     const li = document.createElement('li');
     li.classList.add('app__section-task-list-item');    
     
@@ -34,7 +46,10 @@ function createTask(task) {
 
     const p = document.createElement('p');
     p.textContent = task.description;
-    p.classList.add('app__section-task-list-item-description');
+    p.classList.add('app__section-task-list-item-description'); 
+         
+    const textareaEdit = document.createElement('textarea');
+    textareaEdit.classList.add('app__form-textarea');
 
     const button = document.createElement('button');
     const imageBtn = document.createElement('img');
@@ -43,14 +58,14 @@ function createTask(task) {
     button.classList.add('app_button-edit');
 
     button.onclick = () => {
-        const newDescription = prompt("Qual é o novo nome da tarefa?");
-        if (newDescription) {
-            p.textContent = newDescription;
-            task.description = newDescription;
-            updateTask();
-        }
+        formLabel.textContent = 'Editando tarefa';
+        textarea.value = task.description;
+        
+        setId(task.id);
+
+        formAddTask.classList.remove('hidden');
     };
-    
+
     li.appendChild(svg);
     li.appendChild(p);
     li.appendChild(button);
@@ -59,7 +74,11 @@ function createTask(task) {
 }
 
 btnAddTask.addEventListener('click', () => {
+    formLabel.textContent = 'Adicionando tarefa';
+    textarea.value = '';
     formAddTask.classList.remove('hidden');
+
+    setId(null);
 });
 
 btnCancelTask.addEventListener('click', clearForm);
@@ -67,22 +86,32 @@ btnCancelTask.addEventListener('click', clearForm);
 formAddTask.addEventListener('submit', (event) => {
     event.preventDefault();
 
-    const task = {
-        id: tasks.length + 1,
-        description: textarea.value,
-        completed: false
-    };
-    
-    tasks.push(task);
-    
-    /* Para persistir os dados entre sessões */
-    localStorage.setItem('tasks', JSON.stringify(tasks));
-    /* O localStorage é similar ao sessionStorage, com a diferença de que enquanto os dados do localStorage 
-    não têm tempo de expiração, os dados do sessionStorage são apagados quando a sessão da página termina */
+    const taskId = document.getElementById('id');
+
+    if (taskId.value === null) {
+        console.log('Adicionando tarefa');
+        const task = {
+            id: tasks.length + 1,
+            description: textarea.value,
+            completed: false
+        };
         
-    createTask(task);
-    
-    formAddTask.classList.add('hidden');
-    textarea.value = '';
+        tasks.push(task);
+        
+        /* Para persistir os dados entre sessões */
+        localStorage.setItem('tasks', JSON.stringify(tasks));
+        /* O localStorage é similar ao sessionStorage, com a diferença de que enquanto os dados do localStorage 
+        não têm tempo de expiração, os dados do sessionStorage são apagados quando a sessão da página termina */
+            
+        createTask(task);
+        
+        formAddTask.classList.add('hidden');
+        textarea.value = '';
+    } else {
+        console.log("🚀 ~ formAddTask.addEventListener ~ taskId:", taskId.value)
+
+        console.log('Editando tarefa');
+        
+    }
 });
 
